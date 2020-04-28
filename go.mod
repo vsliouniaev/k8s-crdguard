4359dcc9b909d605c203ad9a09256fa76af95a73
vas <v.sliouniaev@gmail.com>
2020-04-28 21:04:28 +0100

    Flags, err messages, swap logger

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/vsliouniaev/k8s-crdguard
 go 1.14
 
 require (
-	github.com/sirupsen/logrus v1.5.0
+	github.com/go-logr/logr v0.1.0
+	github.com/go-logr/zapr v0.1.1
+	go.uber.org/zap v1.15.0
 	k8s.io/api v0.17.0
 	k8s.io/apimachinery v0.17.0
 	k8s.io/client-go v0.17.0
+	k8s.io/klog v1.0.0
 )
--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
-	log "github.com/sirupsen/logrus"
+	"github.com/go-logr/logr"
+	"github.com/go-logr/zapr"
+	"go.uber.org/zap"
 	"io/ioutil"
 	"k8s.io/api/admission/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -11,26 +14,18 @@ import (
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog"
 	"net/http"
+	"os"
 )
 
 var (
-	deserializer = scheme.Codecs.UniversalDeserializer()
-	client dynamic.Interface
+	deserializer   = scheme.Codecs.UniversalDeserializer()
+	client         dynamic.Interface
+	kubeconfigPath string
+	explicitCrds   mapFlags = make(map[string]bool)
+	log            logr.Logger
 )
-
-func initK8s() {
-	config, err := clientcmd.BuildConfigFromFlags("", "")
-	if err != nil {
-		log.WithError(err).Fatal("error building kubernetes config")
-	}
-
-	c, err := dynamic.NewForConfig(config)
-	if err != nil {
-		log.WithError(err).Fatal("error creating kubernetes client")
-	}
-	client = c
-}
 
 func toAdmissionResponseFailure(message string) *v1beta1.AdmissionResponse {
 	r := &v1beta1.AdmissionResponse{
@@ -40,8 +35,11 @@ func toAdmissionResponseFailure(message string) *v1beta1.AdmissionResponse {
 
 	r.Result.Status = metav1.StatusFailure
 	r.Result.Reason = metav1.StatusReasonInvalid
-	r.Result.Code = http.StatusUnprocessableEntity
+	r.Result.Code = http.StatusConflict
 	r.Result.Message = message
+	r.Result.Details = &metav1.StatusDetails{
+		Causes: []metav1.StatusCause{{Message: message}},
+	}
 
 	return r
 }
@@ -76,37 +74,46 @@ func (c *Crd) getOptimalSchema() (o schema.GroupVersionResource) {
 	return
 }
 
+func shouldCheckCrd(ar v1beta1.AdmissionReview) bool {
+	if len(explicitCrds) == 0 {
+		return true
+	}
+
+	_, ok := explicitCrds[ar.Request.Name]
+	return ok
+}
+
 func validateInstancesNotExists(ar v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
 	if ar.Request.Resource.Group != "apiextensions.k8s.io" {
-		err := fmt.Errorf("expected resource to be apiextensions.k8s.io, but received %v", ar.Request.Resource.Group)
-		log.Error(err)
+		log.Info("expected resource to be apiextensions.k8s.io", "actual", ar.Request.Resource.Group)
 		return toAdmissionResponseFailure("Unexpected resource kind")
 	}
 
 	if ar.Request.Resource.Resource != "customresourcedefinitions" {
-		err := fmt.Errorf("expected resource to be customresourcedefinitions, but received %v", ar.Request.Resource.Resource)
-		log.Error(err)
+		log.Info("expected resource to be customresourcedefinitions", "actual", ar.Request.Resource.Resource)
 		return toAdmissionResponseFailure("Unexpected resource kind")
 	}
 
-	var crd Crd
-	if err := json.Unmarshal(ar.Request.OldObject.Raw, &crd); err != nil {
-		log.WithError(err).WithField("oldObject", string(ar.Request.OldObject.Raw)).Error("cannot unmarshal CRD from spec")
-		return toAdmissionResponseFailure("Cannot unmarshal CRD from spec")
-	}
+	if shouldCheckCrd(ar) {
+		var crd Crd
+		if err := json.Unmarshal(ar.Request.OldObject.Raw, &crd); err != nil {
+			log.Error(err, "cannot unmarshal CRD from spec", "oldObject", string(ar.Request.OldObject.Raw))
+			return toAdmissionResponseFailure("Cannot unmarshal CRD from spec")
+		}
 
-	s := crd.getOptimalSchema()
-	list, err := client.Resource(s).List(metav1.ListOptions{Limit: 1})
-	if err != nil {
-		log.WithError(err).WithField("schema", s).Error("unable to get list of existing CRDs")
-		return toAdmissionResponseFailure("Unable to get list of existing CRDs")
-	}
+		s := crd.getOptimalSchema()
+		list, err := client.Resource(s).List(metav1.ListOptions{Limit: 1})
+		if err != nil {
+			log.Error(err, "unable to get list of existing CRDs", "schema", s)
+			return toAdmissionResponseFailure("Unable to get list of existing CRDs")
+		}
 
-	if len(list.Items) != 0 {
-		log.Infof("found some %s, failing request", ar.Request.Name)
-		return toAdmissionResponseFailure(fmt.Sprintf("There are still some %s in the cluster", ar.Request.Name))
-	} else {
-		log.Debug("found none, allowing delete")
+		if len(list.Items) != 0 {
+			log.V(1).Info("at least on resource exists", "name", ar.Request.Name)
+			return toAdmissionResponseFailure(fmt.Sprintf("There are still some %s in the cluster", ar.Request.Name))
+		} else {
+			log.V(1).Info("found none, allowing delete", "name", ar.Request.Name)
+		}
 	}
 
 	return &v1beta1.AdmissionResponse{Allowed: true}
@@ -121,25 +128,25 @@ func serve(w http.ResponseWriter, r *http.Request, admit admitFunc) {
 	}
 
 	if len(body) == 0 {
-		log.Warn("request has no body")
+		log.Info("request has no body")
 		http.Error(w, "request has no body", http.StatusBadRequest)
 		return
 	}
 
 	contentType := r.Header.Get("Content-Type")
 	if contentType != "application/json" {
-		log.Warn(fmt.Sprintf("invalid Content-Type %s, want `application/json`", contentType))
+		log.Info("invalid Content-Type expected `application/json`", "actual", contentType)
 		http.Error(w, "invalid Content-Type, want `application/json`", http.StatusUnsupportedMediaType)
 		return
 	}
 
-	log.WithField("content", string(body)).Debug("Received request")
+	log.V(2).Info("received request", "content", string(body))
 
 	requestedAdmissionReview := v1beta1.AdmissionReview{}
 	responseAdmissionReview := v1beta1.AdmissionReview{}
 
 	if _, _, err := deserializer.Decode(body, nil, &requestedAdmissionReview); err != nil {
-		log.WithError(err).Warn("unable to deserialize request")
+		log.Error(err, "unable to deserialize request")
 		responseAdmissionReview.Response = toAdmissionResponseFailure("Unable to deserialize request")
 	} else {
 		responseAdmissionReview.Response = admit(requestedAdmissionReview)
@@ -149,20 +156,29 @@ func serve(w http.ResponseWriter, r *http.Request, admit admitFunc) {
 
 	respBytes, err := json.Marshal(responseAdmissionReview)
 
-	log.WithField("content", string(respBytes)).Debug("sending response")
+	log.V(2).Info("sending response", "content", string(respBytes))
 
 	if err != nil {
-		log.WithError(err).Error("cannot serialize response")
+		log.Error(err, "could not serialize response")
 		http.Error(w, fmt.Sprintf("could not serialize response: %v", err), http.StatusInternalServerError)
 	}
 	if _, err := w.Write(respBytes); err != nil {
-		log.WithError(err).Error("cannot write response")
+		log.Error(err, "could not write response")
 		http.Error(w, fmt.Sprintf("could not write response: %v", err), http.StatusInternalServerError)
 	}
 }
 
 func main() {
-	log.SetFormatter(&log.JSONFormatter{})
+
+	flag.StringVar(&kubeconfigPath, "kubeconfig", "", "Path to kubeconfig file: e.g. ~/.kube/kind-config-kind")
+	logDebug := *flag.Bool("log-debug", false, "Whether to enable debug log configuration")
+	certFile := *flag.String("cert-file", "/cert/cert", "Path to certificate file to serve TLS")
+	keyFile := *flag.String("key-file", "/cert/key", "Path to key file to serve TLS")
+	flag.Var(&explicitCrds, "crds", "List of crds to block deletion of. Default will block all CRDs. e.g. 'prometheuses.monitoring.coreos.com'")
+	flag.Parse()
+
+	configLogging(logDebug)
+
 	http.HandleFunc("/healthz", func(w http.ResponseWriter, _ *http.Request) {
 		w.Write([]byte("OK"))
 	})
@@ -172,8 +188,55 @@ func main() {
 	initK8s()
 
 	log.Info("Running on 8443")
-	err := http.ListenAndServeTLS(":8443", "/cert/cert", "/cert/key", nil)
+	err := http.ListenAndServeTLS(":8443", certFile, keyFile, nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Error(err, "failed to start server")
 	}
+}
+
+func configLogging(dev bool) {
+	var config zap.Config
+	if dev {
+		config = zap.NewDevelopmentConfig()
+	} else {
+		config = zap.NewProductionConfig()
+	}
+	zapLog, err := config.Build()
+	if err != nil {
+		panic(err)
+	}
+
+	log = zapr.NewLogger(zapLog)
+
+	// Throw away client-go logs
+	f := &flag.FlagSet{}
+	klog.InitFlags(f)
+	f.Set("log_file", "/dev/null")
+	f.Set("logtostderr", "false")
+}
+
+func initK8s() {
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	if err != nil {
+		log.Error(err, "error building kubernetes config")
+		os.Exit(1)
+	}
+
+	c, err := dynamic.NewForConfig(config)
+	if err != nil {
+		log.Error(err, "error creating kubernetes client")
+		os.Exit(1)
+	}
+	client = c
+}
+
+type mapFlags map[string]bool
+
+func (i *mapFlags) String() string {
+	return ""
+}
+
+func (i *mapFlags) Set(value string) error {
+	(*i)[value] = true
+	return nil
 }
